689114759d17373f5a12ef32fe1fd538176a98bc
zhenzhen qi <zhenzhenqi@gmail.com>
2026-03-07 09:51:18 -0500

    initial commit

--- script/app.js
+++ script/app.js
@@ -1,55 +1,109 @@
-/*
- * ASCII Camera
- * http://idevelop.github.com/ascii-camera/
- *
- * Copyright 2013, Andrei Gheorghe (http://github.com/idevelop)
- * Released under the MIT license
- */
+import { parseGIF, decompressFrames } from 'gifuct-js';
+import GIF from 'gif.js';
+import { ascii } from './ascii.js';
 
-(function() {
-	var asciiContainer = document.getElementById("ascii");
-	var capturing = false;
+(function () {
+	const fileInput = document.getElementById('fileInput');
+	const filterSelect = document.getElementById('filterSelect');
+	const resultContainer = document.getElementById('result');
+	const status = document.getElementById('status');
+	const processBtn = document.getElementById('processBtn');
 
-	camera.init({
-		width: 160,
-		height: 120,
-		fps: 30,
-		mirror: true,
+	// Reusable canvas for processing frames
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
 
-		onFrame: function(canvas) {
-			ascii.fromCanvas(canvas, {
-				// contrast: 128,
-				callback: function(asciiString) {
-					asciiContainer.innerHTML = asciiString;
-				}
-			});
-		},
+	// Helper: Draws ASCII/Braille string to canvas for the GIF encoder
+	const renderAsciiToCanvas = (text, canvas, width, height) => {
+		const lines = text.split('\n');
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, width, height);
+		ctx.fillStyle = "black";
+		ctx.font = "10px monospace";
+		ctx.textBaseline = "top";
+		lines.forEach((line, i) => ctx.fillText(line, 0, i * 10));
+	};
 
-		onSuccess: function() {
-			document.getElementById("info").style.display = "none";
+	const processGIF = async () => {
+		const file = fileInput.files[0];
+		if (!file) return alert("Please select a GIF first.");
 
-			const button = document.getElementById("button");
-			button.style.display = "block";
-			button.onclick = function() {
-				if (capturing) {
-					camera.pause();
-					button.innerText = 'resume';
-				} else {
-					camera.start();
-					button.innerText = 'pause';
-				}
-				capturing = !capturing;
-			};
-		},
+		status.innerText = "Parsing and Converting...";
+		const reader = new FileReader();
 
-		onError: function(error) {
-			// TODO: log error
-		},
+		reader.onload = async (e) => {
+			const gif = parseGIF(e.target.result);
+			const frames = decompressFrames(gif, true);
+			const asciiFrames = [];
 
-		onNotSupported: function() {
-			document.getElementById("info").style.display = "none";
-			asciiContainer.style.display = "none";
-			document.getElementById("notSupported").style.display = "block";
-		}
-	});
+			// 1. Convert all frames
+			for (const frame of frames) {
+				// 1. Calculate a new, smaller size (e.g., 100px wide)
+				const scale = 100 / frame.dims.width;
+				const newWidth = 100;
+				const newHeight = frame.dims.height * scale;
+
+				// 2. Use a temporary small canvas for downsampling
+				const smallCanvas = document.createElement('canvas');
+				smallCanvas.width = newWidth;
+				smallCanvas.height = newHeight;
+				const smallCtx = smallCanvas.getContext('2d');
+
+				// 3. Draw the full frame into the small canvas
+				const tempCanvas = document.createElement('canvas');
+				tempCanvas.width = frame.dims.width;
+				tempCanvas.height = frame.dims.height;
+				tempCanvas.getContext('2d').putImageData(new ImageData(new Uint8ClampedArray(frame.patch), frame.dims.width, frame.dims.height), 0, 0);
+
+				smallCtx.drawImage(tempCanvas, 0, 0, newWidth, newHeight);
+
+				// 4. Run the converter on the DOWNSIZED canvas
+				const converter = (filterSelect.value === 'braille') ? ascii.brailleFromCanvas : ascii.fromCanvas;
+				converter(smallCanvas, { /* ... */ });
+			}
+
+			// 2. Display animation
+			status.innerText = "Conversion complete! Generating download...";
+			playAnimation(asciiFrames);
+
+			// 3. Generate Downloadable GIF
+			createDownloadableGIF(asciiFrames);
+		};
+		reader.readAsArrayBuffer(file);
+	};
+
+	const playAnimation = (frames) => {
+		let i = 0;
+		const animate = () => {
+			resultContainer.innerText = frames[i].text;
+			setTimeout(() => {
+				i = (i + 1) % frames.length;
+				requestAnimationFrame(animate);
+			}, frames[i].delay * 10);
+		};
+		animate();
+	};
+
+	const createDownloadableGIF = (asciiFrames) => {
+		const gif = new GIF({ workers: 2, quality: 10, workerScript: '/gif.worker.js' });
+
+		asciiFrames.forEach(f => {
+			const c = document.createElement('canvas');
+			c.width = f.width; c.height = f.height;
+			renderAsciiToCanvas(f.text, c, f.width, f.height);
+			gif.addFrame(c, { delay: f.delay * 10 });
+		});
+
+		gif.on('finished', (blob) => {
+			const a = document.createElement('a');
+			a.href = URL.createObjectURL(blob);
+			a.download = 'ascii-art.gif';
+			a.click();
+			status.innerText = "Download ready!";
+		});
+		gif.render();
+	};
+
+	processBtn.onclick = processGIF;
 })();
--- script/ascii.js
+++ script/ascii.js
@@ -1,62 +1,65 @@
 // Author: Andrei Gheorghe (http://github.com/idevelop)
+// Braille extension based on 2x2 bitmapping logic
 
-var ascii = (function() {
+export const ascii = (function () {
+	function getPixelBrightness(data, x, y, width, contrastFactor) {
+		var offset = (y * width + x) * 4;
+		var r = data[offset];
+		var g = data[offset + 1];
+		var b = data[offset + 2];
+
+		// Apply contrast
+		r = bound(Math.floor((r - 128) * contrastFactor) + 128, [0, 255]);
+		g = bound(Math.floor((g - 128) * contrastFactor) + 128, [0, 255]);
+		b = bound(Math.floor((b - 128) * contrastFactor) + 128, [0, 255]);
+
+		return (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+	}
+
 	function asciiFromCanvas(canvas, options) {
-		// Original code by Jacob Seidelin (http://www.nihilogic.dk/labs/jsascii/)
-		// Heavily modified by Andrei Gheorghe (http://github.com/idevelop)
-
 		var characters = (" .,:;i1tfLCG08@").split("");
-
 		var context = canvas.getContext("2d");
 		var canvasWidth = canvas.width;
 		var canvasHeight = canvas.height;
-		
 		var asciiCharacters = "";
-
-		// calculate contrast factor
-		// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
 		var contrastFactor = (259 * (options.contrast + 255)) / (255 * (259 - options.contrast));
-
 		var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
-		for (var y = 0; y < canvasHeight; y += 2) { // every other row because letters are not square
+
+		for (var y = 0; y < canvasHeight; y += 2) {
 			for (var x = 0; x < canvasWidth; x++) {
-				// get each pixel's brightness and output corresponding character
-
-				var offset = (y * canvasWidth + x) * 4;
-
-				var color = getColorAtOffset(imageData.data, offset);
-	
-				// increase the contrast of the image so that the ASCII representation looks better
-				// http://www.dfstudios.co.uk/articles/image-processing-algorithms-part-5/
-				var contrastedColor = {
-					red: bound(Math.floor((color.red - 128) * contrastFactor) + 128, [0, 255]),
-					green: bound(Math.floor((color.green - 128) * contrastFactor) + 128, [0, 255]),
-					blue: bound(Math.floor((color.blue - 128) * contrastFactor) + 128, [0, 255]),
-					alpha: color.alpha
-				};
-
-				// calculate pixel brightness
-				// http://stackoverflow.com/questions/596216/formula-to-determine-brightness-of-rgb-color
-				var brightness = (0.299 * contrastedColor.red + 0.587 * contrastedColor.green + 0.114 * contrastedColor.blue) / 255;
-
+				var brightness = getPixelBrightness(imageData.data, x, y, canvasWidth, contrastFactor);
 				var character = characters[(characters.length - 1) - Math.round(brightness * (characters.length - 1))];
-
 				asciiCharacters += character;
 			}
-
 			asciiCharacters += "\n";
 		}
-
 		options.callback(asciiCharacters);
 	}
 
-	function getColorAtOffset(data, offset) {
-		return {
-			red: data[offset],
-			green: data[offset + 1],
-			blue: data[offset + 2],
-			alpha: data[offset + 3]
-		};
+	function brailleFromCanvas(canvas, options) {
+		var context = canvas.getContext("2d");
+		var canvasWidth = canvas.width;
+		var canvasHeight = canvas.height;
+		var brailleCharacters = "";
+		var contrastFactor = (259 * (options.contrast + 255)) / (255 * (259 - options.contrast));
+		var imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+
+		// Iterate in 2x2 blocks as per the a,b,c,d grid logic
+		for (var y = 0; y < canvasHeight; y += 2) {
+			for (var x = 0; x < canvasWidth; x += 2) {
+				// Determine bits (1 if bright, 0 if dark)
+				var a = getPixelBrightness(imageData.data, x, y, canvasWidth, contrastFactor) > 0.5 ? 1 : 0;
+				var b = (x + 1 < canvasWidth) && getPixelBrightness(imageData.data, x + 1, y, canvasWidth, contrastFactor) > 0.5 ? 1 : 0;
+				var c = (y + 1 < canvasHeight) && getPixelBrightness(imageData.data, x, y + 1, canvasWidth, contrastFactor) > 0.5 ? 1 : 0;
+				var d = (x + 1 < canvasWidth && y + 1 < canvasHeight) && getPixelBrightness(imageData.data, x + 1, y + 1, canvasWidth, contrastFactor) > 0.5 ? 1 : 0;
+
+				// Map bits to Braille offset (a=bit0, b=bit1, c=bit2, d=bit3)
+				var code = (a << 0) | (b << 1) | (c << 2) | (d << 3);
+				brailleCharacters += String.fromCharCode(0x2800 + code);
+			}
+			brailleCharacters += "\n";
+		}
+		options.callback(brailleCharacters);
 	}
 
 	function bound(value, interval) {
@@ -64,12 +67,15 @@ var ascii = (function() {
 	}
 
 	return {
-		fromCanvas: function(canvas, options) {
+		fromCanvas: function (canvas, options) {
 			options = options || {};
 			options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
-			options.callback = options.callback || doNothing;
-
 			return asciiFromCanvas(canvas, options);
+		},
+		brailleFromCanvas: function (canvas, options) {
+			options = options || {};
+			options.contrast = (typeof options.contrast === "undefined" ? 128 : options.contrast);
+			return brailleFromCanvas(canvas, options);
 		}
 	};
 })();
